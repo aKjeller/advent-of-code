d63e516e553bcd9b80c10d448139db083ffe221b
aKjeller <44802724+aKjeller@users.noreply.github.com>
2024-12-12 18:20:07 +0100

    feat: y24d12 part2 :(

--- solutions/y24/d12/solution.go
+++ solutions/y24/d12/solution.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	util "github.com/aKjeller/advent-of-code/utilities/go"
+	"slices"
 )
 
 const YEAR = "24"
@@ -32,26 +33,35 @@ func part1(path string) {
 	score := 0
 	for _, region := range regions {
 		for _, p := range region {
-			score += getFences(m, p) * len(region)
+			score += len(getFences(m, make(map[[2]int][]dir), p)) * len(region)
 		}
 	}
 
 	fmt.Printf("part1: %d\n", score)
 }
 
-func getFences(m [][]uint8, p plot) int {
-	fences := 4
-	if p.x+1 >= 0 && p.x+1 < len(m) && p.y >= 0 && p.y < len(m[0]) && m[p.x+1][p.y] == p.plant {
-		fences -= 1
+type dir int
+
+const (
+	NO dir = iota
+	U
+	D
+	L
+	R
+)
+
+func getFences(m [][]uint8, fences map[[2]int][]dir, p plot) map[[2]int][]dir {
+	if p.x+1 < 0 || p.x+1 >= len(m) || p.y < 0 || p.y >= len(m[0]) || m[p.x+1][p.y] != p.plant {
+		fences[[2]int{p.x + 1, p.y}] = append(fences[[2]int{p.x + 1, p.y}], D)
 	}
-	if p.x-1 >= 0 && p.x-1 < len(m) && p.y >= 0 && p.y < len(m[0]) && m[p.x-1][p.y] == p.plant {
-		fences -= 1
+	if p.x-1 < 0 || p.x-1 >= len(m) || p.y < 0 || p.y >= len(m[0]) || m[p.x-1][p.y] != p.plant {
+		fences[[2]int{p.x - 1, p.y}] = append(fences[[2]int{p.x - 1, p.y}], U)
 	}
-	if p.x >= 0 && p.x < len(m) && p.y+1 >= 0 && p.y+1 < len(m[0]) && m[p.x][p.y+1] == p.plant {
-		fences -= 1
+	if p.x < 0 || p.x >= len(m) || p.y+1 < 0 || p.y+1 >= len(m[0]) || m[p.x][p.y+1] != p.plant {
+		fences[[2]int{p.x, p.y + 1}] = append(fences[[2]int{p.x, p.y + 1}], R)
 	}
-	if p.x >= 0 && p.x < len(m) && p.y-1 >= 0 && p.y-1 < len(m[0]) && m[p.x][p.y-1] == p.plant {
-		fences -= 1
+	if p.x < 0 || p.x >= len(m) || p.y-1 < 0 || p.y-1 >= len(m[0]) || m[p.x][p.y-1] != p.plant {
+		fences[[2]int{p.x, p.y - 1}] = append(fences[[2]int{p.x, p.y - 1}], L)
 	}
 	return fences
 }
@@ -81,7 +91,80 @@ func fill(m [][]uint8, x, y int, plant uint8, visited map[plot]bool) []plot {
 }
 
 func part2(path string) {
+	m := util.ToGrid(path)
+
+	visited := make(map[plot]bool)
+
+	var regions [][]plot
+	for i := 0; i < len(m); i++ {
+		for j := 0; j < len(m[i]); j++ {
+			if visited[plot{i, j, m[i][j]}] {
+				continue
+			}
+			regions = append(regions, fill(m, i, j, m[i][j], visited))
+		}
+	}
+
 	score := 0
+	for _, region := range regions {
+		fences := make(map[[2]int][]dir)
+		for _, p := range region {
+			getFences(m, fences, p)
+		}
+
+		var xMin, xMax, yMin, yMax int
+		for k, _ := range fences {
+			xMin = min(xMin, k[0])
+			yMin = min(yMin, k[1])
+			xMax = max(xMax, k[0])
+			yMax = max(yMax, k[1])
+		}
+
+		length := 0
+		for i := xMin; i <= xMax; i++ {
+			for j := yMin; j <= yMax; j++ {
+				if slices.Contains(fences[[2]int{i, j}], U) {
+					for slices.Contains(fences[[2]int{i, j}], U) {
+						j++
+					}
+					length++
+				}
+			}
+		}
+		for i := xMin; i <= xMax; i++ {
+			for j := yMin; j <= yMax; j++ {
+				if slices.Contains(fences[[2]int{i, j}], D) {
+					for slices.Contains(fences[[2]int{i, j}], D) {
+						j++
+					}
+					length++
+				}
+			}
+		}
+
+		for j := yMin; j <= yMax; j++ {
+			for i := xMin; i <= xMax; i++ {
+				if slices.Contains(fences[[2]int{i, j}], R) {
+					for slices.Contains(fences[[2]int{i, j}], R) {
+						i++
+					}
+					length++
+				}
+			}
+		}
+		for j := yMin; j <= yMax; j++ {
+			for i := xMin; i <= xMax; i++ {
+				if slices.Contains(fences[[2]int{i, j}], L) {
+					for slices.Contains(fences[[2]int{i, j}], L) {
+						i++
+					}
+					length++
+				}
+			}
+		}
+
+		score += length * len(region)
+	}
 	fmt.Printf("part2: %d\n", score)
 }
 
